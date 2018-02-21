015b87d938e31575b7a4c221ce8ed6b06be83fa6
Natasha Singh <dukedesi22@gmail.com>
2018-02-21 13:37:15 -0500

    :sparkles: Add etl_module argument to import_data flask cmd

--- dataservice/util/data_import/main.py
+++ dataservice/util/data_import/main.py
@@ -1,8 +1,16 @@
 from importlib import import_module
 
+"""
+Dynamically imports the etl module
+"""
+
 
 def run(module_name):
     prefix_path = 'dataservice.util.data_import'
-    etl_module = import_module("{}.{}.{}".format(prefix_path, module_name,
-                                                 'etl'))
-    etl_module.run()
+    module_path = "{}.{}.{}".format(prefix_path, module_name, 'etl')
+    try:
+        etl_module = import_module(module_path)
+    except ModuleNotFoundError as e:
+        print('ETL module {} does not exist'.format(module_path))
+    else:
+        etl_module.run()
