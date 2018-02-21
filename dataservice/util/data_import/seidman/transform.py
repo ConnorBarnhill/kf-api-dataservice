b620f154110d66ae704d2d967861a7dfeee711df
Natasha Singh <dukedesi22@gmail.com>
2018-02-21 12:39:16 -0500

    :art: Minor clean up and comments

--- dataservice/util/data_import/seidman/transform.py
+++ dataservice/util/data_import/seidman/transform.py
@@ -3,6 +3,7 @@ from dataservice.util.data_import.etl.transform import BaseTransformer
 
 class Transformer(BaseTransformer):
 
-    def __init__(self):
-        from .mappings import mappings_dict
+    def __init__(self, mappings_dict=None):
+        if not mappings_dict:
+            from .mappings import mappings_dict
         super(Transformer, self).__init__(mappings_dict)
