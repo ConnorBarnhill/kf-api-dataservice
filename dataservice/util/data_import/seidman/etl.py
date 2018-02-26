f20d63f4ef20f08089fb0560dd2f9e2142a03988
Natasha Singh <dukedesi22@gmail.com>
2018-02-26 13:49:50 -0500

    :recycle: Update to use default entity types

--- dataservice/util/data_import/seidman/etl.py
+++ dataservice/util/data_import/seidman/etl.py
@@ -4,37 +4,24 @@ from dataservice.util.data_import.seidman.extract import Extractor
 from dataservice.util.data_import.seidman.transform import Transformer
 from dataservice.util.data_import.seidman.load import Loader
 
-ENTITY_TYPES = [
-    'investigator',
-    'study',
-    'study_file',
-    'participant',
-    'family_relationship',
-    'demographic',
-    'diagnosis',
-    'sample',
-    'aliquot',
-    'sequencing_experiment',
-    'genomic_file',
-    'phenotype'
-]
-
 
 def run():
+
     # Extract and combine data
     print('Begin extraction ...')
     e = Extractor()
-    df = e.run()
+    df_dict = e.run()
     print('Completed extraction\n')
 
     # Transform from dataframe to dicts
     print('Begin transformation ...')
     t = Transformer()
-    content = t.run(df, ENTITY_TYPES)
+    content = t.run(df_dict)
     print('Completed transformation\n')
 
     # Load into db via sqlalchemy
     print('Begin loading ...')
+
     l = Loader()
-    l.run(content, entity_types=ENTITY_TYPES)
+    l.run(content)
     print('Completed loading\n')
