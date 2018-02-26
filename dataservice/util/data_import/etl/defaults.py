9d7f13e9cd96d2a4f8ce2d0ca245943876b6f6a5
Natasha Singh <dukedesi22@gmail.com>
2018-02-26 13:33:32 -0500

    :recycle: Move default entity list into defaults.py

---
+++ dataservice/util/data_import/etl/defaults.py
@@ -0,0 +1,14 @@
+DEFAULT_ENTITY_TYPES = [
+    'investigator',
+    'study',
+    'study_file',
+    'participant',
+    'demographic',
+    'diagnosis',
+    'sample',
+    'aliquot',
+    'sequencing_experiment',
+    'genomic_file',
+    'family_relationship',
+    'phenotype'
+]
