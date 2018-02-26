be0fc671b36008e51cd9c908a32874ba8f451c65
Natasha Singh <dukedesi22@gmail.com>
2018-02-26 13:33:32 -0500

    :recycle: Move default entity list into defaults.py

--- dataservice/util/data_import/schiffman/etl.py
+++ dataservice/util/data_import/schiffman/etl.py
@@ -1,26 +1,13 @@
+import os
 from pprint import pprint
 
 from dataservice.util.data_import.schiffman.extract import Extractor
 from dataservice.util.data_import.schiffman.transform import Transformer
 from dataservice.util.data_import.schiffman.load import Loader
 
-ENTITY_TYPES = [
-    'investigator',
-    'study',
-    'study_file',
-    'participant',
-    'family_relationship',
-    'demographic',
-    'diagnosis',
-    'sample',
-    'aliquot',
-    'sequencing_experiment',
-    'genomic_file',
-    'phenotype'
-]
-
 
 def run():
+
     # Extract and combine data
     print('Begin extraction ...')
     e = Extractor()
@@ -30,12 +17,12 @@ def run():
     # Transform from dataframe to dicts
     print('Begin transformation ...')
     t = Transformer()
-    content = t.run(df_dict, ENTITY_TYPES)
+    content = t.run(df_dict)
     print('Completed transformation\n')
 
     # Load into db via sqlalchemy
     print('Begin loading ...')
 
     l = Loader()
-    l.run(content, entity_types=ENTITY_TYPES)
+    l.run(content)
     print('Completed loading\n')
