34503fc8ecb8b447cb652b0085a1208bf1b4d518
Natasha Singh <dukedesi22@gmail.com>
2018-02-28 12:42:51 -0500

    :bug: Fix handling of missing foreign keys

--- dataservice/util/data_import/etl/load.py
+++ dataservice/util/data_import/etl/load.py
@@ -92,6 +92,12 @@ class BaseLoader(object):
             if '_links' in params:
                 linked_entities = params['_links']
                 for linked_entity, _params in linked_entities.items():
+                    if not (('source_fk_col' in _params) and
+                            ('target_fk_col' in params)):
+                        print('Error loading {0}. Missing foreign key info.'
+                              ' Check mappings for {0}'
+                              .format(entity_type))
+                        continue
                     source_fk_col = _params['source_fk_col']
                     target_fk_col = _params['target_fk_col']
                     fk_value = self.entity_id_map[linked_entity][source_fk_col]
