dd0441c9eaefa49aff9d14ebb3f2cccf3cd9bee2
Natasha Singh <dukedesi22@gmail.com>
2018-04-30 20:34:41 -0400

    :recycle: Chung - refactor to use yaml config

--- dataservice/util/data_import/chung/etl.py
+++ dataservice/util/data_import/chung/etl.py
@@ -1,25 +1,8 @@
+from dataservice.util.data_import.etl.base_etl import BaseETLModule
 from dataservice.util.data_import.chung.extract import Extractor
-from dataservice.util.data_import.chung.transform import Transformer
-from dataservice.util.data_import.chung.load import Loader
-from dataservice.util.data_import.etl import etl
-
-STUDY_EXT_ID = 'phs001110'
-
-ex = Extractor()
-tm = Transformer()
-ld = Loader()
 
 
-def run():
-    """
-    Run extract, transfrom, load modules for a particular dataset
-    """
-    etl.run(ex, tm, ld)
+class ETLModule(BaseETLModule):
 
-
-def drop_data(study_external_id=STUDY_EXT_ID):
-    """
-    Delete all data associated with a study identified by the study's
-    external_id
-    """
-    etl.drop_data(ld, study_external_id)
+    def __init__(self, config):
+        super().__init__(config, Extractor(config))
