3d8ed72ae657f4ef2f9125a0a6ee918c3ad39a86
Natasha Singh <dukedesi22@gmail.com>
2018-04-30 20:34:41 -0400

    :recycle: Chung - refactor to use yaml config

--- dataservice/util/data_import/chung/transform.py
+++
@@ -1,9 +0,0 @@
-from dataservice.util.data_import.etl.transform import BaseTransformer
-
-
-class Transformer(BaseTransformer):
-
-    def __init__(self, mappings_dict=None):
-        if not mappings_dict:
-            from .mappings import mappings_dict
-        super(Transformer, self).__init__(mappings_dict)
