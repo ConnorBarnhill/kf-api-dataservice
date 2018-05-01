fde8e1d2a575ac576685b2be7e27c2ad4bcabe8b
Natasha Singh <dukedesi22@gmail.com>
2018-04-30 21:54:15 -0400

    :recycle: Seq centers - refactor to use yaml config

--- dataservice/util/data_import/seq_centers/etl.py
+++ dataservice/util/data_import/seq_centers/etl.py
@@ -1,26 +1,10 @@
+from dataservice.util.data_import.etl.base_etl import BaseETLModule
 from dataservice.util.data_import.seq_centers.extract import Extractor
-from dataservice.util.data_import.seq_centers.transform import Transformer
 from dataservice.util.data_import.seq_centers.load import Loader
-from dataservice.util.data_import.etl import etl
-
-ex = Extractor()
-tm = Transformer()
-ld = Loader()
 
 
-def run():
-    """
-    Run extract, transfrom, load modules for a particular dataset
-    """
-    etl.run(ex, tm, ld)
+class ETLModule(BaseETLModule):
 
-
-def drop_data():
-    """
-    Delete all sequencing centers
-    """
-    from dataservice.api.sequencing_center.models import SequencingCenter
-    from dataservice.extensions import db
-
-    SequencingCenter.query.delete()
-    db.session.commit()
+    def __init__(self, config):
+        super().__init__(config, extractor=Extractor(config),
+                         loader=Loader(config))
